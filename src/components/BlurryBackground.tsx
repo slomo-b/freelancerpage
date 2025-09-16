92c82f470efa7191a8e4f77c6ada2d21791a195c
[dyad] <ravrider@protonmail.com>
2025-09-16 21:51:12 +0200

    [dyad] Injecting more color with a vibrant background and a new violet accent color - wrote 2 file(s)

--- src/components/BlurryBackground.tsx
+++ src/components/BlurryBackground.tsx
@@ -4,8 +4,8 @@ export function BlurryBackground() {
       aria-hidden="true"
       className="fixed inset-0 -z-50 overflow-hidden"
     >
-      <div className="absolute left-[max(50%,25rem)] top-0 h-[60rem] w-[60rem] -translate-x-1/2 rounded-full bg-blue-200/20 blur-3xl" />
-      <div className="absolute left-1/2 top-[max(20%,40rem)] h-[60rem] w-[60rem] -translate-x-1/2 rounded-full bg-indigo-200/20 blur-3xl" />
+      <div className="absolute left-[max(50%,25rem)] top-0 h-[60rem] w-[60rem] -translate-x-1/2 rounded-full bg-violet-300/20 blur-3xl" />
+      <div className="absolute left-1/2 top-[max(20%,40rem)] h-[60rem] w-[60rem] -translate-x-1/2 rounded-full bg-cyan-300/20 blur-3xl" />
     </div>
   );
 }
